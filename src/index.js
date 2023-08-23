5615c44614509b3280f8a8d7bbc740ed3c9d00bf
domm <dommferger@gmail.com>
2023-08-23 16:41:43 -0400

    Idk anymore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+let taskMaster = [];
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // Make sure the page isn't re-directed
+  document.querySelector("#create-task-form").addEventListener("submit", e => {
+          e.preventDefault();
+       //console.log(e.target["new-task-description"].value)
+      let newTask = e.target["new-task-description"].value
+      const newTaskButton = document.createElement('button');
+      taskMaster.push(newTaskButton);
+      newTaskButton.textContent = newTask;
+      document.getElementById("list").appendChild(newTaskButton);
+        console.log(taskMaster);
+  })
 });
+
+
+
+
+                                      /* Some Ideas
+          > Create an empty array taskMaster[];
+          > Have that array fill up with each 'task' that's been submitted by the user
+          > I think the array needs to be accessed by the HTML portion
+          > Have it so anytime a user submits something new it's pushed (push()) into the array
+          > when that button array is clicked it's sliced (slice());
+          > Probably have to do a for i = 0; etc to get the array # in the list to slice
+          */
+         
